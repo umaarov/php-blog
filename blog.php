7ca1037d6d00863aa0516df7428571e15508726c
Ismoiljon <hs.umarov21@gmail.com>
2024-08-14 16:53:07 +0500

    push 7

--- blog.php
+++ blog.php
@@ -1,8 +1,16 @@
 <?php
 $title = "Blog";
 require "includes/header.php";
+require 'database.php';
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+$statement->execute();
+
+$posts = $statement->fetchAll();
+
+// var_dump($posts[0]);
 // if (isset($_SESSION["post-created"])) {
-//     $message = $_SESSION["post-created"];
+// $message = $_SESSION["post-created"];
 // }
 
 ?>
@@ -32,6 +40,27 @@ require "includes/header.php";
             </div>
         <?php endif; ?>
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <?php foreach ($posts as $post): ?>
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <title>Placeholder</title>
+                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                        </svg>
+                        <div class="card-body">
+                            <h5 class="card-text"><?= $post["title"]; ?></h5>
+                            <p class="card-text"><?= $post["body"]; ?></p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small class="text-body-secondary"><?= $post["created_at"]; ?></small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
 
         </div>
     </div>
